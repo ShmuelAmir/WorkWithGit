71b16c71a035cc2a47ff01e2fa209488363c7001
ShmuelAmir <shmulik5777@gmail.com>
2021-03-03 12:44:25 +0200

    Point3D

--- Shmuel&Chananel/src/primitives/Point3D.java
+++ Shmuel&Chananel/src/primitives/Point3D.java
@@ -30,6 +30,21 @@ public class Point3D {
 	 * @return
 	 */
 	public Vector subtract(Point3D point) {
-		
+		return new Vector(this.x.coord - point.x.coord, this.y.coord - point.y.coord, this.z.coord - point.z.coord);
+	}
+	
+	public Point3D add(Vector vector) {
+		return Point3D(this.x.coord + vector.head.x.coord, this.y.coord + vector.head.y.coord, this.z.coord + vector.head.z.coord);
+	}
+	
+	public double distanceSquared(Point3D point) {
+		double xSquared = (this.x.coord - point.x.coord) * (this.x.coord - point.x.coord);
+		double ySquared = (this.y.coord - point.y.coord) * (this.y.coord - point.y.coord);
+		double zSquared = (this.z.coord - point.z.coord) * (this.z.coord - point.z.coord);
+		return xSquared + ySquared + zSquared;
+	}
+	
+	public double distance(Point3D point) {
+		return Math.sqrt(distanceSquared(point));
 	}
 }
