6121b95e206dd5ef4a59e2eaccb98ac6388b7a32
zchananel <chananelz@gmail.com>
2021-03-13 20:25:25 +0200

    tihod ex 2

--- Shmuel&Chananel/src/unittests/CylinderTests.java
+++ Shmuel&Chananel/src/unittests/CylinderTests.java
@@ -25,14 +25,17 @@ public class CylinderTests {
 		Cylinder c = new Cylinder(r, 1, 2);
 		
 		// ============ Equivalence Partitions Tests ==============
+		// TC01: test normal in side
 		assertEquals("Bad normal to cylinder", new Vector(1, 0, 0), c.getNormal(new Point3D(1, 0, 1)));
+		// TC02:test normal in base A
 		assertEquals("Bad normal to cylinder", new Vector(0, 0, -1), c.getNormal(new Point3D(0, 0, 0)));
+		// TC03:test normal in base B
 		assertEquals("Bad normal to cylinder", new Vector(0, 0, 1), c.getNormal(new Point3D(0, 0, 2)));
 		
 		// =============== Boundary Values Tests ==================
-		// TC10: Suture point (normal vector according to the base of the tube)
+		// TC10: test normal in suture point (normal vector defined according to the base of the tube)
 		assertEquals("Bad normal to cylinder", new Vector(0, 0, -1), c.getNormal(new Point3D(1, 0, 0)));
-		// TC11: 
+		// TC11: test normal in suture point (normal vector defined according to the base of the tube)
 		assertEquals("Bad normal to cylinder", new Vector(0, 0, 1), c.getNormal(new Point3D(1, 0, 2)));
 	}
 
--- Shmuel&Chananel/src/unittests/Point3DTests.java
+++ Shmuel&Chananel/src/unittests/Point3DTests.java
@@ -16,11 +16,14 @@ import primitives.Vector;
  */
 public class Point3DTests {
 
+	// ============ Equivalence Partitions Tests ==============
 	/**
 	 * Test method for {@link primitives.Point3D#subtract(primitives.Point3D)}.
 	 */
 	@Test
 	public void testSubtract() {
+		
+		// TC01: Point + Vector
 		Point3D p1 = new Point3D(1, 2, 3);
 		assertTrue("Point + Vector does not work correctly", Point3D.ZERO.equals(p1.add(new Vector(-1, -2, -3))));
 	}
@@ -30,6 +33,7 @@ public class Point3DTests {
 	 */
 	@Test
 	public void testAdd() {
+		// TC02: Point - Vector
 		Point3D p1 = new Point3D(1, 2, 3);
 		assertTrue("Point - Point does not work correctly",new Vector(1, 1, 1).equals(new Point3D(2, 3, 4).subtract(p1)));
 	}
