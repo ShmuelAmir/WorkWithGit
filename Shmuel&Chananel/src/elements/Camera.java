c80586ad4fd6a7dffc31a41e6f467508d98eccfd
ShmuelAmir <shmulik5777@gmail.com>
2021-04-23 15:49:34 +0300

    debug ex4

--- Shmuel&Chananel/src/elements/Camera.java
+++ Shmuel&Chananel/src/elements/Camera.java
@@ -4,6 +4,7 @@
 package elements;
 
 import static primitives.Util.*;
+
 import primitives.*;
 
 /**
@@ -11,40 +12,42 @@ import primitives.*;
  *
  */
 public class Camera {
-	private Point3D  p0;
+	private Point3D p0;
 	private Vector vUp;
 	private Vector vTo;
 	private Vector vRight;
 	private double width;
 	private double height;
 	private double distance;
-	
-	
+
 	/**
 	 * @return the p0
 	 */
 	public Point3D getP0() {
 		return p0;
 	}
+
 	/**
 	 * @return the vUp
 	 */
 	public Vector getvUp() {
 		return vUp;
 	}
+
 	/**
 	 * @return the vTo
 	 */
 	public Vector getvTo() {
 		return vTo;
 	}
+
 	/**
 	 * @return the vRight
 	 */
 	public Vector getvRight() {
 		return vRight;
 	}
-	
+
 	/**
 	 * @param p0
 	 * @param vUp
@@ -54,41 +57,40 @@ public class Camera {
 		this.p0 = p0;
 		this.vUp = vUp.normalized();
 		this.vTo = vTo.normalized();
-		if (isZero(vUp.dotProduct(vTo))) 
+		if (! isZero(vUp.dotProduct(vTo)))
 			throw new IllegalArgumentException("///////");
 		vRight = vTo.crossProduct(vUp).normalized();
-		
+
 	}
-	
-	public Camera setViewPlaneSize(double width, double height)
-	{
+
+	public Camera setViewPlaneSize(double width, double height) {
 		this.width = width;
 		this.height = height;
 		return this;
 	}
-	
-	public Camera setDistance(double distance)
-	{
+
+	public Camera setDistance(double distance) {
 		this.distance = distance;
 		return this;
 	}
-	
-	public Ray constructRayThroughPixel(int nX, int nY, int j, int i)
-	{
-		return null;
+
+	public Ray constructRayThroughPixel(int nX, int nY, int j, int i) {
+		Point3D pCenter = p0.add(vTo.scale(distance));
+		
+		double rX = width / nX;
+		double rY = height / nY;
+		
+		double xJ = (j - ((nX - 1) / 2.0)) * rX;
+		double yI = (-1) * (i - ((nY - 1) / 2.0)) * rY;
+		
+		Point3D pIJ = pCenter;
+		if (xJ != 0) 
+			pIJ = pIJ.add(vRight.scale(xJ));
+		if (yI != 0) 
+			pIJ = pIJ.add(vUp.scale(yI));
+		
+		return new Ray(p0, pIJ.subtract(p0));
+//		return new Ray(p0, p0.subtract(pIJ));
 	}
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 }
--- Shmuel&Chananel/src/unittests/CameraRayConstructionTest.java
+++ Shmuel&Chananel/src/unittests/CameraRayConstructionTest.java
@@ -11,7 +11,6 @@ import elements.Camera;
 import geometries.*;
 import primitives.*;
 
-
 /**
  * @author user1
  *
@@ -19,29 +18,65 @@ import primitives.*;
 public class CameraRayConstructionTest {
 
 	@Test
-	public void test() {
-		
-		// TC01: 3X3 Sphere 
-		Camera camera = new Camera(Point3D.ZERO, new Vector(0, 1, 0), new Vector(0, 0 , -1));
+	public void testSphere() {
+
+		// TC01: 3X3 Sphere
+		Camera camera = new Camera(Point3D.ZERO, new Vector(0, 1, 0), new Vector(0, 0, -1));
 		camera.setDistance(1).setViewPlaneSize(3, 3);
 		Sphere sphere = new Sphere(new Point3D(0, 0, -3), 1);
 
-		
-		assertEquals("ray sphere 2 point intersections",2,countIntersections(camera, sphere));
-		
+		assertEquals("ray sphere 2 point intersections", 2, countIntersections(camera, sphere));
+
 		// TC02: 3X3 Sphere
-		camera = new Camera(new Point3D(0, 0, 0.5), new Vector(0, 1, 0), new Vector(0, 0 , -1));
+		camera = new Camera(new Point3D(0, 0, 0.5), new Vector(0, 1, 0), new Vector(0, 0, -1));
 		camera.setDistance(1).setViewPlaneSize(3, 3);
 		sphere = new Sphere(new Point3D(0, 0, -2.5), 2.5);
 
-		assertEquals("ray sphere 2 point intersections",2,countIntersections(camera, sphere));
-		
-		
+		assertEquals("ray sphere 18 point intersections", 18, countIntersections(camera, sphere));
+
 		// TC03: 3X3 Sphere
+		sphere = new Sphere(new Point3D(0, 0, -2), 2);
+		assertEquals("ray sphere 10 point intersections", 10, countIntersections(camera, sphere));
+
 		// TC04: 3X3 Sphere
+		sphere = new Sphere(new Point3D(0, 0, 1.5), 4);
+		assertEquals("ray sphere 10 point intersections", 9, countIntersections(camera, sphere));
+
 		// TC05: 3X3 Sphere
-		assertEquals("Bad ray", new Ray(Point3D.ZERO, new Vector(-2, -2, 10)),
-				camera.setViewPlaneSize(6, 6).constructRayThroughPixel(3, 3, 0, 0));
+		sphere = new Sphere(new Point3D(0, 0, 1), 0.5);
+		assertEquals("ray sphere 10 point intersections", 0, countIntersections(camera, sphere));
+	}
+
+	@Test
+	public void testPlain() {
+		Camera camera = new Camera(Point3D.ZERO, new Vector(0, 1, 0), new Vector(0, 0, -1));
+		camera.setDistance(1).setViewPlaneSize(3, 3);
+
+		// TC01: 3X3 Plane
+		Plane plane = new Plane(new Point3D(0, 0, -5), new Vector(0, 0, 1));
+		assertEquals("ray sphere 9 point intersections", 9, countIntersections(camera, plane));
+
+		// TC02: 3X3 Plane
+		plane = new Plane(new Point3D(-1, 2, 0), new Point3D(-1, -2, 0), new Point3D(-0.5, 2, 2));
+		assertEquals("ray sphere 9 point intersections", 9, countIntersections(camera, plane));
+
+		// TC03: 3X3 Plane
+		plane = new Plane(new Point3D(-4, 2, 0), new Point3D(-4, -2, 0), new Point3D(-0.5, 2, 2));
+		assertEquals("ray sphere 6 point intersections", 6, countIntersections(camera, plane));
+	}
+
+	@Test
+	public void testTriangle() {
+		Camera camera = new Camera(Point3D.ZERO, new Vector(0, 1, 0), new Vector(0, 0, -1));
+		camera.setDistance(1).setViewPlaneSize(3, 3);
+
+		// TC01: 3X3 Sphere
+		Triangle triangle = new Triangle(new Point3D(0, 1, -2), new Point3D(-1, -1, -2), new Point3D(1, -1, -2));
+		assertEquals("ray sphere 1 point intersections", 1, countIntersections(camera, triangle));
+
+		// TC02: 3X3 Sphere
+		triangle = new Triangle(new Point3D(0, 20, -2), new Point3D(-1, -1, -2), new Point3D(1, -1, -2));
+		assertEquals("ray sphere 2 point intersections", 2, countIntersections(camera, triangle));
 	}
 
 	/**
@@ -53,21 +88,16 @@ public class CameraRayConstructionTest {
 	public int countIntersections(Camera camera, Intersectable shape) {
 		int sum = 0;
 		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j <3 ; j++) {
-				var intersection = shape.findIntersections(camera.constructRayThroughPixel(3, 3, j, i));  ///???  באמת צריך?
+			for (int j = 0; j < 3; j++) {
+				var intersection = shape.findIntersections(camera.constructRayThroughPixel(3, 3, j, i)); /// ??? באמת
+																											/// צריך?
 				if (intersection != null) {
 					sum += intersection.size();
-				}	
+				}
 			}
 		}
-		
+
 		return sum;
 	}
-	
-	
-	
 
-	
-	
-	
 }
