f42e13008a4236e958e7684b554fd251c9264be4
ShmuelAmir <shmulik5777@gmail.com>
2021-04-28 09:46:12 +0300

    ex5 start

---
+++ Shmuel&Chananel/src/unittests/ImageWriterTest.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package unittests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import primitives.Color;
+import renderer.ImageWriter;
+
+/**
+ * @author shmulik
+ *
+ */
+public class ImageWriterTest {
+
+	/**
+	 * Test method for {@link renderer.ImageWriter#writeToImage()}.
+	 */
+	@Test
+	public void testWriteToImage() {
+		ImageWriter imageWriter = new ImageWriter("FirstImage", 800, 500);
+		Color blue = new Color(java.awt.Color.BLUE);
+		Color red = new Color(java.awt.Color.RED);
+		
+		for (int j = 0; j < 800; j++) {
+			for (int i = 0; i < 500; i++) {
+				if (i % 50 == 0 || j % 50 == 0)
+					imageWriter.writePixel(j, i, red);
+				else
+					imageWriter.writePixel(j, i, blue);
+			}
+		}
+		
+		imageWriter.writeToImage();
+	}
+
+}
