d5ef41a6dac315c2d5d86c915b9619c73700c428
ShmuelAmir <shmulik5777@gmail.com>
2021-04-29 09:38:54 +0300

    s1

--- Shmuel&Chananel/src/renderer/RayTracerBasic.java
+++ Shmuel&Chananel/src/renderer/RayTracerBasic.java
@@ -26,12 +26,12 @@ public class RayTracerBasic extends RayTracerBase {
 	@Override
 	public Color traceRay(Ray ray) {
 		List<Point3D> intersections = scene.geometries.findIntersections(ray);
-		if (intersections == null) 
+		if (intersections == null)
 			return scene.background;
 		Point3D closestPoint = ray.getClosestPoint(intersections);
 		return calcColor(closestPoint);
 	}
-	
+
 	private Color calcColor(Point3D point) {
 		return scene.ambientLight.getIntensity();
 	}
