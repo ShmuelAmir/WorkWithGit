1d8736586219c85ea4bd96056da308186ae3c2da
ShmuelAmir <shmulik5777@gmail.com>
2021-05-26 13:51:54 +0300

    commons

--- Shmuel&Chananel/src/renderer/RaysBeam.java
+++ Shmuel&Chananel/src/renderer/RaysBeam.java
@@ -24,7 +24,10 @@ public class RaysBeam {
 
 		
 		/**
-		 * @param edge
+		 * construct the BlackBoard - center point, edge size and axes.
+		 * 
+		 * @param r - distance between center to edge
+		 * @param ray - for direction and center point
 		 */
 		public BlackBoard(double r, Ray ray) {
 			this.edge = 2 * r * DISTANCE;
@@ -32,6 +35,8 @@ public class RaysBeam {
 			center = ray.getPoint(DISTANCE);
 			
 			Vector vTo = ray.getDir();
+			
+			// finding orthogonal vectors
 			double x = vTo.dotProduct(new Vector(1, 0, 0));
 			double y = vTo.dotProduct(new Vector(0, 1, 0));
 			double z = vTo.dotProduct(new Vector(0, 0, 1));
@@ -48,19 +53,26 @@ public class RaysBeam {
 			}
 			
 			yAxis = xAxis.crossProduct(vTo);
+			
+			// the length of the vectors
 			xAxis.scale(edge / 2);
 			yAxis.scale(edge / 2);
 		}
 		
 	}
 	
-	Random rand = new Random();
-	Point3D p0;
-	BlackBoard blackBoard;
-	List<Ray> rays = new LinkedList<>();
+	private Point3D p0; // starting point for all rays
+	private BlackBoard blackBoard; // the ending point for all rays
+	private Random rand = new Random(); // for the move
+	private List<Ray> rays = new LinkedList<>(); // to return
+	
+	/**
+	 * number of rays to generate and return
+	 */
 	private static final int NUM_OF_RAYS = 4;
 	
 	/**
+	 * construct the start point and the blackBoard according to the ray.
 	 * 
 	 * @param ray
 	 */
@@ -70,21 +82,23 @@ public class RaysBeam {
 	}
 	
 	/**
+	 * generate rays from p0 to the scope of the black board randomly.
 	 * 
-	 * @return
+	 * @return list of rays
 	 */
 	public List<Ray> generateRays(){
 		Point3D point;
 		while (rays.size() < NUM_OF_RAYS) {
+			// move the center point along the axes of the blackBoard
 			point = blackBoard.center.add(blackBoard.xAxis.scale(rand.nextDouble()))
 					.add(blackBoard.yAxis.scale(rand.nextDouble()));
 			
+			// construct a new ray
 			rays.add(new Ray(p0, point.subtract(p0)));
 			
 		}
 		
 		return rays;
 	}
-	
 	
 }
