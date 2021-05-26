ad054533c4ba17a14b27fc10b98d0b6226e23f0d
ShmuelAmir <shmulik5777@gmail.com>
2021-05-26 13:36:48 +0300

    delete

--- Shmuel&Chananel/src/primitives/Ray.java
+++ Shmuel&Chananel/src/primitives/Ray.java
@@ -127,14 +127,4 @@ public class Ray {
 		return p0.toString() + " " + dir.toString();
 	}
 	
-	////---------------------------------------------------////
-	
-	public Point3D getDirHead() {
-		return dir.getHead();
-	}
-	
-//	public Ray getOrthogonalRay() {
-//		dir.getHead();
-//		return null;
-//	}
 }
